ea1cab2227cd1f45056f86d1e3687646804e1942
zemul <zhouzemiao@ihuman.com>
2023-04-20 16:24:12 +0800

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module tableConvert
+
+go 1.20
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/atotto/clipboard v0.1.4
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.7.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/atotto/clipboard"
+	"os"
+	"strings"
+)
+
+var html string
+
+func main() {
+	fmt.Println("Paste your HTML Table data  here ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		html += input
+		if strings.Contains(input, "</table>") {
+			break
+		}
+	}
+
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
+	if err != nil {
+		panic(err)
+	}
+
+	var md strings.Builder
+
+	doc.Find("thead tr th").Each(func(i int, s *goquery.Selection) {
+		text := s.Text()
+		md.WriteString("| ")
+		md.WriteString(text)
+		md.WriteString(" ")
+	})
+	md.WriteString("|\n")
+
+	doc.Find("tbody tr").Each(func(i int, s *goquery.Selection) {
+		s.Find("td").Each(func(j int, s2 *goquery.Selection) {
+			text := s2.Text()
+			md.WriteString("| ")
+			md.WriteString(text)
+			md.WriteString(" ")
+		})
+		md.WriteString("|\n")
+	})
+
+	fmt.Println()
+	fmt.Println(md.String())
+	err = clipboard.WriteAll(md.String())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("🍺 write clipboard success")
+}
